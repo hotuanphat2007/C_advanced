6522c8ef0848d17f1ce9110c768c8cdc532f9d20
Tuan Phat <hotuanphat2007@gmail.com>
2023-02-11 08:44:36 +0700

    add c files

---
+++ main.c
@@ -0,0 +1,20 @@
+// git init: initialize the folder to be upload to github
+// git status: check the status of all file in the folder.
+////////////////git add
+
+/*
+
+    thu muc dang lam viec               staging area                                    commited
+    main.c                -->git add --> main.c  --> git comit -m "add main.c file"
+
+
+
+
+*/
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("test github");
+    return 0;
+}
